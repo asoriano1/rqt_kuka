3ed6b6a423aafb4d301d3ee2eca1587f76be1a1a
Angel Soriano <asoriano@robotnik.es>
2018-04-05 12:47:22 +0200

    actions for button pressing programmed and gripper settings

--- src/rqt_kuka/robotnik_rqt_kuka_node.py
+++ src/rqt_kuka/robotnik_rqt_kuka_node.py
@@ -5,13 +5,15 @@ import inspect
 
 import rospy
 import rospkg
+import time 
 
 from qt_gui.plugin import Plugin
 from python_qt_binding import loadUi
 from python_qt_binding.QtWidgets import QWidget, QDialog, QFileDialog, QMessageBox, QTableWidgetItem
-from std_msgs.msg import Bool
+from std_msgs.msg import Bool, Float64, Float32
 from sensor_msgs.msg import JointState
-from robotnik_msgs.srv import home, set_odometry
+from robotnik_msgs.srv import home, set_odometry, set_kuka_pose
+from robotnik_msgs.msg import Kuka_pose
 from geometry_msgs.msg import Pose, Point, Quaternion
 
 import yaml
@@ -28,12 +30,12 @@ STATE_PLACED=5
 STATE_HOMING=6
 
 TOOL_HOMED=False
-
+KUKA_AUT=False
 #service names:
-srv_name_move_abs_fast='/setKukaAbsFast'
-srv_name_move_abs_slow='/setKukaAbs'
-srv_name_move_rel_fast='/setKukaRelFast'
-srv_name_move_rel_slow='/setKukaRel'
+srv_name_move_abs_fast='/kuka_robot/setKukaAbsFast'
+srv_name_move_abs_slow='/kuka_robot/setKukaAbs'
+srv_name_move_rel_fast='/kuka_robot/setKukaRelFast'
+srv_name_move_rel_slow='/kuka_robot/setKukaRel'
 srv_tool_homing='/kuka_tool/robotnik_base_hw/home'
 srv_finger_set_pose='/kuka_tool_finger_node/set_odometry' #robotnik_msgs.set.odometry
 
@@ -41,24 +43,31 @@ srv_finger_set_pose='/kuka_tool_finger_node/set_odometry' #robotnik_msgs.set.odo
 topic_cart_pose_kuka='/kuka_robot/cartesian_pos_kuka'
 topic_kuka_moving='/kuka_robot/kuka_moving'
 topic_tool_weight='/kuka_gauges/phidget_load/load_mean'
-topic_current='current'
+topic_current='/kuka_tool/robotnik_base_hw/current'
 
 #Prepick Pose # tf.transformations.quaternion_from_euler(0, 0, th)
 #Prepick_Pose=Pose(Point(100, 100, 100), Quaternion(0, 0, 0, 1))
-Prepick_Pose_x=100
-Prepick_Pose_y=100
-Prepick_Pose_z=100
+Prepick_Pose_x=1725
+Prepick_Pose_y=-142
+Prepick_Pose_z=1253.57
 Prepick_Pose_theta=0
-
+Prepick_Pose_a=-66
+Prepick_Pose_b=0
+Prepick_Pose_c=178
 #Preplace Pose
 #Preplace_Pose=Pose(Point(400, 400, 100), Quaternion(0, 0, 0, 1))
-Preplace_Pose_x=400
-Preplace_Pose_y=400
-Preplace_Pose_z=100
+Preplace_Pose_x=108
+Preplace_Pose_y=1727
+Preplace_Pose_z=1337
 Preplace_Pose_theta=0
-
+Preplace_Pose_a=-2
+Preplace_Pose_b=0
+Preplace_Pose_c=178
 #RollerBench Pose
 RollerBench_Pose=Pose(Point(200, 200, 100), Quaternion(0, 0, 0, 1))
+pos_z_kuka=0.0
+weight_read=0.0
+weight_empty=0.0
 
 class RqtKuka(Plugin):
 
@@ -95,9 +104,9 @@ class RqtKuka(Plugin):
 
         #Buttons
         self._widget.PrePick_Button.pressed.connect(self.press_prepick_button)
-        #self._widget.PickTest_Button.pressed.connect(self.press_select_yaml)
-        #self._widget.Picked_Button.pressed.connect(self.press_select_yaml)
-        self._widget.Placed_Button.pressed.connect(self.press_preplace_button)
+        self._widget.PickTest_Button.pressed.connect(self.press_picktest_button)
+        self._widget.Picked_Button.pressed.connect(self.press_picked_button)
+        self._widget.Placed_Button.pressed.connect(self.press_placed_button)
         self._widget.Gripper_Homing_Button.pressed.connect(self.press_tool_homming)
         self._widget.Apply_Button.pressed.connect(self.press_apply_button)
 
@@ -109,13 +118,13 @@ class RqtKuka(Plugin):
         rospy.Subscriber(topic_kuka_moving, Bool, self.callback_moving)
 
         #subscriber to robot pose
-        rospy.Subscriber(topic_cart_pose_kuka, JointState, self.callback_robot_pose)     
+        rospy.Subscriber(topic_cart_pose_kuka, Kuka_pose, self.callback_robot_pose)     
 
         #subscriber to tool weight detected
-        rospy.Subscriber(topic_tool_weight, JointState, self.callback_tool_weight)     
+        rospy.Subscriber(topic_tool_weight, Float64, self.callback_tool_weight)     
 
         #subscriber to tool current
-        rospy.Subscriber(topic_current, JointState, self.callback_current) 
+        rospy.Subscriber(topic_current, Float32, self.callback_current) 
         
         # Show _widget.windowTitle on left-top of each plugin (when 
         # it's set in _widget). This is useful when you open multiple 
@@ -146,35 +155,45 @@ class RqtKuka(Plugin):
         self._widget.Placed_Button.setEnabled(True)
 
     def callback_moving(self, data):
-        print 'CB:moving_received:',data.data
-        if data.data == True:
-            self._widget.mode_label.setText("AUTOMATIC")
-            #selt._widget.mode_label.setStyleSheet(\ncolor: rgb(255, 0, 0))
-        else:
-            self._widget.mode_label.setText("MANUAL")
+		global KUKA_AUT
+		print 'CB:moving_received:',data.data
+		if data.data == True:
+							KUKA_AUT=True
+							self._widget.mode_label.setText("AUTOMATIC")
+							#selt._widget.mode_label.setStyleSheet(\ncolor: rgb(255, 0, 0))
+		else:
+			KUKA_AUT=False
+			self._widget.mode_label.setText("MANUAL")
 
     def callback_robot_pose(self, data):
-        print 'CB:robot_pose_received',data
-        self._widget.robot_pose_x.setText(str(data.position[0]))
-        self._widget.robot_pose_y.setText(str(data.position[1]))
-        self._widget.robot_pose_z.setText(str(data.position[2]))
-        self._widget.robot_pose_a.setText(str(data.position[3]))
-        self._widget.robot_pose_b.setText(str(data.position[4]))
-        self._widget.robot_pose_c.setText(str(data.position[5]))
+		global pos_z_kuka
+		print 'CB:robot_pose_received',data
+		self._widget.robot_pose_x.setText(str(data.x))
+		self._widget.robot_pose_y.setText(str(data.y))
+		self._widget.robot_pose_z.setText(str(data.z))
+		self._widget.robot_pose_a.setText(str(data.A))
+		self._widget.robot_pose_b.setText(str(data.B))
+		self._widget.robot_pose_c.setText(str(data.C))
+		pos_z_kuka=data.z
 
     def callback_tool_weight(self, data):
-        print 'CB:tool_weight_received',data
-        self._widget.weight_lcdNumber.display(8)
+		global weight_empty, weight_read
+		print 'CB:tool_weight_received',data
+		weight_read=data.data
+		weight_no_tool=data.data-weight_empty
+		self._widget.weight_lcdNumber.display(weight_no_tool)
 
     def callback_current(self, data):
         print 'CB:current_received',data
-        self._widget.tool_force_lcdNumber.display(8)
+        self._widget.tool_force_lcdNumber.display(data.data)
 
     def press_tool_homming(self):
         #Call tool homing method
+        global weight_empty, weight_read
         try:
-            homing_service = rospy.ServiceProxy(srv_tool_homing, home)            
+            homing_service = rospy.ServiceProxy(srv_tool_homing, home)           
             ret = homing_service()
+            weight_empty=weight_read
             if ret == True:
                 TOOL_HOMED=True
                 self._widget.info_label.setText("Service tool homing call done")
@@ -185,23 +204,59 @@ class RqtKuka(Plugin):
     def press_prepick_button(self):
         #Call service to move robot to prepick pose
         try:
-            prepick_service = rospy.ServiceProxy(srv_name_move_abs_fast, set_odometry)            
-            ret = prepick_service(Prepick_Pose_x, Prepick_Pose_y, Prepick_Pose_z, Prepick_Pose_theta)
+            prepick_service = rospy.ServiceProxy(srv_name_move_abs_fast, set_kuka_pose)            
+            ret = prepick_service(Prepick_Pose_x, Prepick_Pose_y, Prepick_Pose_z, Prepick_Pose_a, Prepick_Pose_b, Prepick_Pose_c)
             if ret == True:
-                CURRENT_STATE=1
+                CURRENT_STATE=STATE_MOVING_TO_PREPICK
         except rospy.ServiceException, e:
             print "Service call failed: %s"%e 
 
-    def press_preplace_button(self):
-        #Call service to move robot to prepick pose
-        try:
-            prepick_service = rospy.ServiceProxy(srv_name_move_abs_fast, set_odometry)            
-            ret = prepick_service(Prepick_Pose_x, Prepick_Pose_y, Prepick_Pose_z, Prepick_Pose_theta)
-            if ret == True:
-                CURRENT_STATE=3
-        except rospy.ServiceException, e:
-            print "Service call failed: %s"%e
-    
+    def press_placed_button(self):
+		global KUKA_AUT, pos_z_kuka
+        #Call service to move robot up and then to the pre-pick pose, should be fast
+		try:
+			placed_rel_service = rospy.ServiceProxy(srv_name_move_rel_slow, set_kuka_pose)
+			ret_rel=placed_rel_service(0, 0, -pos_z_kuka+Prepick_Pose_z+50, 0, 0, 0)
+			KUKA_AUT=True
+			while KUKA_AUT: time.sleep(0.05)
+			placed_abs_service = rospy.ServiceProxy(srv_name_move_abs_fast, set_kuka_pose)
+			ret = placed_abs_service(Prepick_Pose_x, Prepick_Pose_y, Prepick_Pose_z, Prepick_Pose_a,Prepick_Pose_b,Prepick_Pose_c)
+			#ret=placed_rel_service(0, 0, -100, 0, 0, 0)
+			if ret == True:
+				CURRENT_STATE=3
+		except rospy.ServiceException, e:
+			print "Service call failed: %s"%e
+
+    def press_picked_button(self):
+		global KUKA_AUT, pos_z_kuka
+		#Call service to move robot up and then to pre place pose, should be slow
+		try:
+			picked_rel_service = rospy.ServiceProxy(srv_name_move_rel_slow, set_kuka_pose)
+			ret_rel=picked_rel_service(0, 0, -pos_z_kuka+Preplace_Pose_z+50, 0, 0, 0)
+			KUKA_AUT=True
+			while KUKA_AUT: time.sleep(0.05)
+			picked_abs_service = rospy.ServiceProxy(srv_name_move_abs_fast, set_kuka_pose)
+			ret = picked_abs_service(Preplace_Pose_x, Preplace_Pose_y, Preplace_Pose_z, Preplace_Pose_a,Preplace_Pose_b,Preplace_Pose_c)
+			#ret=placed_rel_service(0, 0, -100, 0, 0, 0)
+			if ret == True:
+				CURRENT_STATE=3
+		except rospy.ServiceException, e:
+			print "Service call failed: %s"%e
+            
+    def press_picktest_button(self):
+		global KUKA_AUT
+		try:
+			#placed_abs_service = rospy.ServiceProxy(srv_name_move_abs_slow, set_kuka_pose)
+			placed_rel_service = rospy.ServiceProxy(srv_name_move_rel_slow, set_kuka_pose)
+			ret_rel=placed_rel_service(0, 0, 20, 0, 0, 0)
+			KUKA_AUT=True
+			while KUKA_AUT: time.sleep(0.05)
+			#ret = placed_abs_service(Prepick_Pose_x, Prepick_Pose_y, Prepick_Pose_z, Prepick_Pose_a,Prepick_Pose_b,Prepick_Pose_c)
+			if ret_rel == True:
+					CURRENT_STATE=3
+		except rospy.ServiceException, e:
+			print "Service call failed: %s"%e
+			    
     def press_apply_button(self):
         Prepick_Pose_x=self._widget.prepick_x.toPlainText()
         Prepick_Pose_y=self._widget.prepick_y.toPlainText()
@@ -221,7 +276,8 @@ class RqtKuka(Plugin):
     
     def calibre_selected(self, index):
         print 'Selected:', index
-        #self.set_current_arm()
+        #self.set_current_arm() NO DEBERIA ENTRAR AQUI SIN EL HOMING
+        gripper_trasl_service = rospy.ServiceProxy(srv_finger_set_pose,set_odometry)
         if index == 0:
             print 'No gripper selected'
             self.desactivate_buttons()
@@ -232,15 +288,28 @@ class RqtKuka(Plugin):
         if index == 1:            
             print 'Set gripper to 100mm'
             #TODO: Set gripper to 100mm
+            tras_from_homing=0.21-0.1;
+            #ret=gripper_trasl_service(tras_from_homing,0,0,0)
+            #if ret == False:
+				#print 'Set gripper to 100mm: OUT OF RANGE'
         elif index == 2:
             print 'Set gripper to 140mm'
             #TODO: Set gripper to 140mm
+            tras_from_homing=0.21-0.14;
+            ret=gripper_trasl_service(tras_from_homing,0,0,0)
+            if ret == False:
+				print 'Set gripper to 140mm: OUT OF RANGE'
         elif index == 3:
             print 'Set gripper to 160mm'
             #TODO: Set gripper to 160mm
+            tras_from_homing=0.21-0.16;
+            ret=gripper_trasl_service(tras_from_homing,0,0,0)
+            if ret == False:
+				print 'Set gripper to 140mm: OUT OF RANGE'
         elif index == 4:
             print 'Set gripper to 270mm'
             #TODO: Set gripper to 270mm
+            ret=gripper_trasl_service(0.01,0,0,0)
 
         
     def shutdown_plugin(self):
