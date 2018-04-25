739d4e0c81e858293738111b0f3473ee78ae6c2d
asoriano1 <asoriano@robotnik.es>
2018-04-25 10:33:19 +0200

    path changed

--- scripts/robotnik_kuka.sh
+++ scripts/robotnik_kuka.sh
@@ -4,5 +4,7 @@ source /opt/ros/kinetic/setup.bash
 source ~/kuka_catkin_ws/devel/setup.bash
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 
-rqt
+rqt --standalone rqt_kuka &
+sleep 1
+rviz
 sleep 3000
