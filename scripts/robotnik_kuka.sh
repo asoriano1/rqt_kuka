1d2602dd019fe6974ce8588c8fea8ba7f0289973
asoriano1 <asoriano@robotnik.es>
2018-05-07 08:58:39 +0200

    added ROS_MASTER_URI and ROS_IP

--- scripts/robotnik_kuka.sh
+++ scripts/robotnik_kuka.sh
@@ -2,6 +2,9 @@
 echo "Testin"
 source /opt/ros/kinetic/setup.bash
 source ~/kuka_catkin_ws/devel/setup.bash
+export ROS_MASTER_URI="http://192.168.1.10:11311"
+export ROS_IP="192.168.1.11"
+
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 
 rqt --standalone rqt_kuka &
